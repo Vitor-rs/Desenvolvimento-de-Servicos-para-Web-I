111560a46d94029481c4a3e167553e50c7bb4351
VitorGit93 <vitor.santos9@estudante.ifms.edu.br>
2023-06-23 12:50:15 -0400

    Correção de importação

--- projeto-spring/registro-aula/src/main/java/edu/escola/sistemaedu/registroaula/dadoacademico/DadoAcademicoController.java
+++ projeto-spring/registro-aula/src/main/java/edu/escola/sistemaedu/registroaula/dadoacademico/DadoAcademicoController.java
@@ -1,7 +1,6 @@
-package edu.escola.sistemaedu.registroaula.dadoacademico.controller;
+package edu.escola.sistemaedu.registroaula.dadoacademico;
 
-import edu.escola.sistemaedu.registroaula.dadoacademico.DadoAcademicoDto;
-import edu.escola.sistemaedu.registroaula.dadoacademico.DadoAcademicoService;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
