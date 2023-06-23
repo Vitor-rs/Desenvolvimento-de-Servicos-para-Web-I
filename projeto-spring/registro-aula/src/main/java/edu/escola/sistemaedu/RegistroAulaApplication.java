6be9d12b021eaab5c28f6825342e10662bf3bd39
Vitor Santos <vitor.santos9@estudante.ifms.edu.br>
2023-06-22 22:21:19 -0400

    :wrench: Adicionando arquivos de configuração e documentação

---
+++ projeto-spring/registro-aula/src/main/java/edu/escola/sistemaedu/RegistroAulaApplication.java
@@ -0,0 +1,13 @@
+package edu.escola.sistemaedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistroAulaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegistroAulaApplication.class, args);
+    }
+
+}
